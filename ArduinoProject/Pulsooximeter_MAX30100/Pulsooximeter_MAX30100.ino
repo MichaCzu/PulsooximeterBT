3c8806cdbb0e38bd459f61006c44254e1c32f77c
Michael Czuba <czuba.mich@gmail.com>
2020-05-23 19:41:21 +0200

    Add bluetooth connection

--- ArduinoProject/Pulsooximeter_MAX30100/Pulsooximeter_MAX30100.ino
+++ ArduinoProject/Pulsooximeter_MAX30100/Pulsooximeter_MAX30100.ino
@@ -1,13 +1,18 @@
 #include <Wire.h>
 #include "MAX30100_PulseOximeter.h"
+#include <SoftwareSerial.h>
+
 #define OKRES_WYSWIETLANIA 200
-float tetno;
-PulseOximeter max30100;
+SoftwareSerial BTSerial(10, 11); // RX, TX
+float tetno, saturacja;
 uint32_t poprzedniOdczyt = 0;
+PulseOximeter max30100;
 
 void setup() {
   max30100.begin();
-  max30100.setIRLedCurrent(MAX30100_LED_CURR_11MA); 
+  max30100.setIRLedCurrent(MAX30100_LED_CURR_11MA);
+  Serial.begin(9600);
+  BTSerial.begin(57600);
 }
 
 void loop() {
@@ -15,7 +20,10 @@ void loop() {
   if (millis() - poprzedniOdczyt > OKRES_WYSWIETLANIA) {
     saturacja = max30100.getSpO2();
     tetno = max30100.getHeartRate();
-
+    
+    Serial.println(String(String(tetno)+";"+String(saturacja)));
+    BTSerial.println(String(String(tetno)+";"+String(saturacja)));
+    
     poprzedniOdczyt = millis();
   }
 }
